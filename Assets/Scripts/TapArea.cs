efc16cc813773e1ce55432363f39e41408535253
muslimah00 <muslimah.mbs@gmail.com>
2021-09-12 22:37:54 +0700

    first commit

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+ 
+//Muslimah 439
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+
+    public void OnPointerDown (PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+"# chapter2_IncrementalGame" 
+"# Chapter2_IncrementalGame" 
+"# Chapter2_IncerementalGame" 
+"# TugasChapter2_IncrementalGame" 
+"# TugasChapter2_IncrementalGame" 
+"# TugasChapter2_IncrementalGame" 
+"# IncrementalGame_c2" 
